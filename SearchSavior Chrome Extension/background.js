26bd940c510f535ee836633d8e2eba414a4c5be4
Akshay R. Kapadia <akshayrajankapadia@gmail.com>
2019-03-16 15:11:21 -0400

    Removed extra code in background.js

--- SearchSavior Chrome Extension/background.js
+++ SearchSavior Chrome Extension/background.js
@@ -4,17 +4,6 @@
 
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-});
-
 var contextMenuItem = {
   "id": "SearchSavior",
   "title": "SearchSavior",
