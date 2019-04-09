0d2a1384e32061b1990edef3ef8c107d04e8c36e
Akshay R. Kapadia <akshayrajankapadia@gmail.com>
2019-04-09 14:16:49 -0400

    Made code look better

--- popup.js
+++ popup.js
@@ -3,27 +3,32 @@
 // found in the LICENSE file.
 
 document.addEventListener('DOMContentLoaded', function() {
-    var searchbar = document.getElementById('searchbar');
-    var answers = document.getElementById("answers");
-    var searchButton = document.getElementById("searchButton");
-    searchButton.addEventListener("click", function() {
-      var q = searchbar.value;
-      chrome.storage.sync.set({"stored_q": q});
-      answers.src = "https://ui.customsearch.ai/hosted-page?customconfig=d0014089-d8da-4575-aeaf-5a409ea341d1&version=latest&market=en-US&q=" + q;
+  var searchbar = document.getElementById('searchbar');
+  var answers = document.getElementById("answers");
+  var searchButton = document.getElementById("searchButton");
+  searchButton.addEventListener("click", function() {
+    var q = searchbar.value;
+    chrome.storage.sync.set({
+      "stored_q": q
     });
-    searchbar.addEventListener("keyup", function(e) {
-    	if (e.keyCode == 13) {
-        searchButton.click();
-      }
-  	});
+    answers.src = "https://ui.customsearch.ai/hosted-page?customconfig=d0014089-d8da-4575-aeaf-5a409ea341d1&version=latest&market=en-US&q=" + q;
+  });
+  searchbar.addEventListener("keyup", function(e) {
+    if (e.keyCode == 13) {
+      searchButton.click();
+    }
+  });
 });
-
-chrome.tabs.executeScript({code: "window.getSelection().toString();"}, function(selection) {
+chrome.tabs.executeScript({
+  code: "window.getSelection().toString();"
+}, function(selection) {
   var searchbar = document.getElementById("searchbar");
   var q = "";
   if ((selection != null) && (selection[0] != "")) {
-  	q = selection[0];
-    chrome.storage.sync.set({"stored_q": q});
+    q = selection[0];
+    chrome.storage.sync.set({
+      "stored_q": q
+    });
     searchbar.value = q;
     var searchbar = document.getElementById('searchbar');
     var answers = document.getElementById("answers");
